ba60ef804de7a8d499681353e93c2ac296d8fbf2
Thomas Errico-Seaman <thomas.errico.seaman@gmail.com>
2026-09-06 17:24:02 -0400

    First commit for assignment 1

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+// HelloWorld.cpp
+// Author: Mohammad Pivezhandi <pivezhandi@wayne.edu>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Prints a greeting. CSC 4110 Assignment 1
+    cout << "Testing git merge!" << endl;
+    return 0;
+}
